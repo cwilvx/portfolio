c4990b1e63c514b47fff377b3880bfb9c37bd677
mungai-njoroge <geoffreymungai45@gmail.com>
2024-01-19 19:25:37 +0300

    add nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/styles/index.scss"],
-  modules: ["@nuxt/image-edge", "@nuxt/content"],
+  modules: ["@nuxt/image-edge", "@nuxt/content", "@nuxthq/studio"],
   app: {
     head: {
       charset: "utf-16",
